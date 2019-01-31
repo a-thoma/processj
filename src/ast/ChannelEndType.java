849cb44de053230e63448444c586201204d0557e
Matt Pedersen <matt.pedersen@unlv.edu>
2019-01-31 15:42:38 -0800

    ChannelEndType changed to hold a channel, all type nodes have implement equal, equivalent and assignmentcompatible

--- src/ast/ChannelEndType.java
+++ src/ast/ChannelEndType.java
@@ -14,16 +14,16 @@ public class ChannelEndType extends Type {
     private int shared;
     private int end;
 
-    public ChannelEndType(int shared, Type baseType, int end) {
-        super(baseType);
+    public ChannelEndType(int shared, ChannelType channelType, int end) {
+        super(channelType);
         this.shared = shared;
         this.end = end;
         nchildren = 1;
-        children = new AST[] { baseType };
+        children = new AST[] { channelType };
     }
 
     public String typeName() {
-        return "chan<" + baseType() + ">." + (isRead() ? "read" : "write");
+        return "chan<" + channel().baseType() + ">." + (isRead() ? "read" : "write");
     }
 
     // TODO: perhaps the base type of a channel end type ought to be a channel ;->
@@ -33,7 +33,7 @@ public class ChannelEndType extends Type {
     }
 
     public String signature() {
-        return "{" + baseType().signature() + ";" + (isRead() ? "?" : "!");
+        return "{" + channel().baseType().signature() + ";" + (isRead() ? "?" : "!");
     }
 
     public String toString() {
@@ -52,26 +52,52 @@ public class ChannelEndType extends Type {
         return end == WRITE_END;
     }
 
+    public ChannelType channel() {
+	return (ChannelType)children[0];
+    }
+
     public Type baseType() {
-        return (Type) children[0];
+        return channel().baseType();
     }
 
     public <S extends Object> S visit(Visitor<S> v) {
         return v.visitChannelEndType(this);
     }
 
-    @Override
-    public boolean equal(Type t) {
-        return false;
+   
+    // ********************
+    // Type Related Stuff
+    // ********************
+
+    @Override public boolean isChannelEndType() {
+	return true;
     }
 
-    @Override
-    public boolean equivalent(Type t) {
-        return false;
+    // α = ChannelEnd(δ, end1) ∧ β = ChannelEnd(γ, end2)
+    // α =T β ⇔ ChannelEnd?(α) ∧ ChannelEnd?(β) ∧ Channel?(δ) ∧ Channel?(γ) ∧ (end1 = end2)
+    @Override public boolean typeEqual(Type t) {
+	// ChannelEnd?(α) ∧ ChannelEnd?(β)
+	if (!t.isChannelEndType())
+	    return false;
+	ChannelEndType other = (ChannelEndType)t; 
+	// (end1 = end2) 
+	if (this.end != other.end)
+	    return false;
+	// Channel?(δ) =T Channel?(γ)
+	// TODO: This should operate on channels
+	return baseType().typeEqual(other.baseType());
     }
 
-    @Override
-    public boolean assignmentCompatible(Type t) {
+    // α∼T β ⇔ α=T β
+    @Override public boolean typeEquivalent(Type t) {
+	return this.typeEqual(t);
+    }
+
+    // if α = ChannelEnd(δ, end1) ∧ β = ChannelEnd(γ, end2)
+    // α :=T β ⇔ (end1 = end2) ∧
+    // ((Protocol?(δ) ∧ Protocol?(γ) ∧ (δ ≤T γ)) ∨ (¬Protocol?(δ) ∧ ¬Protocol?(γ) ∧ (δ =T γ))
+    @Override public boolean typeAssignmentCompatible(Type t) {
+	// TODO
         return false;
     }
 }
