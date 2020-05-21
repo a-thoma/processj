18ebbe1408de7a85fc809c72556898c4d0a6ebdc
a-thoma <athoma@protonmail.com>
2020-05-20 17:24:32 -0700

    fixed issue with not finding libs due to looking for non-existing (until now, that is) include/C++ folder, changed settings, started grammar template rework

--- src/utilities/Settings.java
+++ src/utilities/Settings.java
@@ -10,8 +10,8 @@ public class Settings {
     
     public static String absolutePath = new File("").getAbsolutePath() + "/";
     public static String includeDir = "include";
-     public static Language language = Language.JVM;
-//    public static Language language = Language.CPLUS;
+//     public static Language language = Language.JVM;
+    public static Language language = Language.CPLUS;
     public static boolean ansiColor = false;
     public static final String IMPORT_FILE_EXTENSSION = "pj";
     public static final String LEGACY_FILE_EXTENSION = "processj";
