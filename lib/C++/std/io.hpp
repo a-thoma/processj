ddea176c625420b36ae35ad07869a80ef022b5c6
a-thoma <athoma@protonmail.com>
2020-07-09 18:46:54 -0700

    finished and fleshed out most of library -- need to figure out what a byte would be/how that will work -- it might just go to a char by default...

--- lib/C++/std/io.hpp
+++ lib/C++/std/io.hpp
@@ -2,13 +2,92 @@
 #define PJ_IO_HPP
 namespace io
 {
-    void println()
+    static void println()
     {
         std::cout << std::endl;
     }
 
-    void println(std::string s)
+    // TODO: what is a byte in c++, a char...? flesh this out
+    // static void println(std::byte b)
+    // {
+    //     std::cout << b << std::endl;
+    // }
+
+    static void println(short s)
     {
         std::cout << s << std::endl;
+    }
+
+    static void println(int i)
+    {
+        std::cout << i << std::endl;
+    }
+
+    static void println(char c)
+    {
+        std::cout << c << std::endl;
+    }
+
+    static void println(long l)
+    {
+        std::cout << l << std::endl;
+    }
+
+    static void println(float f)
+    {
+        std::cout << f << std::endl;
+    }
+
+    static void println(double d)
+    {
+        std::cout << d << std::endl;
+    }
+
+    static void println(std::string s)
+    {
+        std::cout << s << std::endl;
+    }
+
+    // no line feed
+    
+    // TODO: what is a byte in c++, a char...? flesh this out
+    // static void print(std::byte b)
+    // {
+    //     std::cout << b;
+    // }
+
+    static void print(short s)
+    {
+        std::cout << s;
+    }
+
+    static void print(int i)
+    {
+        std::cout << i;
+    }
+
+    static void print(char c)
+    {
+        std::cout << c;
+    }
+
+    static void print(long l)
+    {
+        std::cout << l;
+    }
+
+    static void print(float f)
+    {
+        std::cout << f;
+    }
+
+    static void print(double d)
+    {
+        std::cout << d;
+    }
+
+    static void print(std::string s)
+    {
+        std::cout << s;
     }
 }
