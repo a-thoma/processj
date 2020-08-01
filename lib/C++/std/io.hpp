ef67447c59b7cb218848577af9e99af7c6e4f630
a-thoma <athoma@protonmail.com>
2020-07-31 20:17:30 -0700

    perhaps a template is best here

--- lib/C++/std/io.hpp
+++ lib/C++/std/io.hpp
@@ -8,51 +8,57 @@ public:
         std::cout << std::endl;
     }
 
+    template<typename T>
+    static void println(T v)
+    {
+        std::cout << v << std::endl;
+    }
+
     // TODO: what is a byte in c++, a char...? flesh this out
     // static void println(std::byte b)
     // {
     //     std::cout << b << std::endl;
     // }
 
-    static void println(const short s)
-    {
-        std::cout << s << std::endl;
-    }
+    // static void println(const short s)
+    // {
+    //     std::cout << s << std::endl;
+    // }
 
-    static void println(const int i)
-    {
-        std::cout << i << std::endl;
-    }
+    // static void println(const int i)
+    // {
+    //     std::cout << i << std::endl;
+    // }
 
-    static void println(const char c)
-    {
-        std::cout << c << std::endl;
-    }
+    // static void println(const char c)
+    // {
+    //     std::cout << c << std::endl;
+    // }
 
-    static void println(const long l)
-    {
-        std::cout << l << std::endl;
-    }
+    // static void println(const long l)
+    // {
+    //     std::cout << l << std::endl;
+    // }
 
-    static void println(const float f)
-    {
-        std::cout << f << std::endl;
-    }
+    // static void println(const float f)
+    // {
+    //     std::cout << f << std::endl;
+    // }
 
-    static void println(const double d)
-    {
-        std::cout << d << std::endl;
-    }
+    // static void println(const double d)
+    // {
+    //     std::cout << d << std::endl;
+    // }
 
-    static void println(const char* s)
-    {
-        std::cout << s << std::endl;
-    }
+    // static void println(const char* s)
+    // {
+    //     std::cout << s << std::endl;
+    // }
 
-    static void println(const std::string s)
-    {
-        std::cout << s << std::endl;
-    }
+    // static void println(const std::string s)
+    // {
+    //     std::cout << s << std::endl;
+    // }
 
     // no line feed
     
@@ -62,39 +68,39 @@ public:
     //     std::cout << b;
     // }
 
-    static void print(short s)
-    {
-        std::cout << s;
-    }
+    // static void print(short s)
+    // {
+    //     std::cout << s;
+    // }
 
-    static void print(int i)
-    {
-        std::cout << i;
-    }
+    // static void print(int i)
+    // {
+    //     std::cout << i;
+    // }
 
-    static void print(char c)
-    {
-        std::cout << c;
-    }
+    // static void print(char c)
+    // {
+    //     std::cout << c;
+    // }
 
-    static void print(long l)
-    {
-        std::cout << l;
-    }
+    // static void print(long l)
+    // {
+    //     std::cout << l;
+    // }
 
-    static void print(float f)
-    {
-        std::cout << f;
-    }
+    // static void print(float f)
+    // {
+    //     std::cout << f;
+    // }
 
-    static void print(double d)
-    {
-        std::cout << d;
-    }
+    // static void print(double d)
+    // {
+    //     std::cout << d;
+    // }
 
-    static void print(std::string s)
-    {
-        std::cout << s;
-    }
+    // static void print(std::string s)
+    // {
+    //     std::cout << s;
+    // }
 };
 #endif
