ad25cfdacb8396dad5c6e1b7548ba620df4299ca
a-thoma <thomaa1@unlv.nevada.edu>
2020-08-19 13:56:21 -0700

    incorporated new array implementation, need to implement in template/codegenerator, and then fix the print rewriter to handle n-dimensional arrays

--- src/cppruntime/include/runtime/pj_array.hpp
+++ src/cppruntime/include/runtime/pj_array.hpp
@@ -43,7 +43,7 @@ namespace pj_runtime
             {
                 std::ostringstream message;
                 message << "Invalid Argument: index "
-                        << idx << " is out of bounds (length is "
+                        << idx << " is out of bounds (size is "
                         << length << ")."
                         << std::endl;
                 throw std::invalid_argument(message.str());
@@ -58,7 +58,74 @@ namespace pj_runtime
             {
                 std::ostringstream message;
                 message << "Invalid Argument: index "
-                        << idx << " is out of bounds (length is "
+                        << idx << " is out of bounds (size is "
+                        << length << ")."
+                        << std::endl;
+                throw std::invalid_argument(message.str());
+            }
+
+            return m_array[idx];
+        }
+
+
+    private:
+        T* m_array;
+    };
+
+    template<class T>
+    class pj_md_array
+    {
+    public:
+        int32_t length;
+
+        pj_md_array()
+        {
+            m_array = nullptr;
+        }
+
+        pj_md_array(std::initializer_list<T> values)
+        :length(values.size())
+        {
+            m_array = new T[length];
+
+            std::copy(values.begin(), values.end(), m_array);
+        }
+
+        ~pj_md_array()
+        {
+            if(m_array)
+            {
+                for(int32_t i = 0; i < length; ++i)
+                {
+                    delete m_array[i];
+                }
+                delete[] m_array;
+                m_array = nullptr;
+            }
+        }
+
+        T& operator[](int32_t idx)
+        {
+            if(idx > length)
+            {
+                std::ostringstream message;
+                message << "Invalid Argument: index "
+                        << idx << " is out of bounds (size is "
+                        << length << ")."
+                        << std::endl;
+                throw std::invalid_argument(message.str());
+            }
+
+            return m_array[idx];
+        }
+
+        const T& operator[](int32_t idx) const
+        {
+            if(idx > length)
+            {
+                std::ostringstream message;
+                message << "Invalid Argument: index "
+                        << idx << " is out of bounds (size is "
                         << length << ")."
                         << std::endl;
                 throw std::invalid_argument(message.str());
